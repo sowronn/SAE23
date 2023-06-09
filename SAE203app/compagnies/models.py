d544fa9e753886ee3b6b6aeddea2cd07e30d2c3c
ROCKS-IT <joueur_3690@protonmail.com>
2023-06-09 15:26:27 +0200

    sui

--- SAE203app/compagnies/models.py
+++ SAE203app/compagnies/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class compagnie(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    nom = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    pays = models.CharField(max_length=100)
+
