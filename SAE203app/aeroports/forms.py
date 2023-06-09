83701b686efcc2ed07667848a07fdfbb2b636a4a
sowronn <101816645+sowronn@users.noreply.github.com>
2023-06-09 16:13:28 +0200

    aeroports en cours

---
+++ SAE203app/aeroports/forms.py
@@ -0,0 +1,12 @@
+from django.forms import ModelForm
+from django.utils.translation import gettext_lazy as _
+from . import models
+
+class aeroportsForm(ModelForm):
+    class Meta :
+        model = models.aeroports
+        fields = ('nom', 'pays')
+        labels = {
+            'nom' : _('Nom'),
+            'pays' : _('Pays')
+        }
--- SAE203app/aeroports/models.py
+++ SAE203app/aeroports/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class aeroports(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    nom = models.CharField(max_length=100)
+    pays = models.CharField(max_length=100)
+
+    def __str__(self):
+        chaine = f"l'aeroport: {self.nom} dans le pays: {self.pays} d'ID: {self.id}"
+        return chaine
