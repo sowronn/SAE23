f3a76ad2fb0950f915b141c737a9e5ca29dd09cb
sowronn <101816645+sowronn@users.noreply.github.com>
2023-06-09 13:50:37 +0200

    6 app

---
+++ SAE203app/avions/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AvionsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'avions'
---
+++ SAE203app/types_avions/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TypesAvionsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'types_avions'
