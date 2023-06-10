002b1c341c406c3ed75ad0367ab5f0169ff0781f
sowronn <sowronn.data@gmail.com>
2023-06-10 17:03:47 +0200

    create aeroport operationnelle

--- SAE203app/aeroports/urls.py
+++ SAE203app/aeroports/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns =[
     path("ajout/", views.ajout),
     path("traitement/", views.traitement),
+    path("", views.index),
+    path("affiche/<int:id>/", views.affiche)
 
 ]
--- SAE203app/aeroports/views.py
+++ SAE203app/aeroports/views.py
@@ -1,20 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from .forms import aeroportsForm
+from . import models
 
 # Create your views here.
+
 
 def ajout(request):
     if request.method == "POST":
         form = aeroportsForm(request)
         return render(request, "aeroports/ajout.html", {"form": form})
-    else :
+    else:
         form = aeroportsForm()
         return render(request, "aeroports/ajout.html", {"form": form})
+
 
 def traitement(request):
     lform = aeroportsForm(request.POST)
     if lform.is_valid():
         aeroports = lform.save()
-        return render(request, "aeroports/traitement.html", {"aeroports" : aeroports})
-    else :
+        return HttpResponseRedirect("/SAE203app")
+    else:
         return render(request, "aeroports/ajout.html", {"form": lform})
+
+
+def index(request):
+    liste = list(models.aeroports.objects.all())
+    return render(request, "Aeroports/index.html", {"liste": liste})
+
+
+def affiche(request, id):
+    aeroports = models.aeroports.objects.get( pk = id)
+    return render(request,"aeroport/affiche.html",{"aeroports": aeroports})
