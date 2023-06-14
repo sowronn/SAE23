5f241dd0ef82bc01097a5ed71d1b98929cf0c12d
sowronn <101816645+sowronn@users.noreply.github.com>
2023-06-14 14:10:21 +0200

    TOUT est ok

--- SAE203app/aeroports/models.py
+++ SAE203app/aeroports/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class aeroports(models.Model):
+class aeroports(models.Model) :
     id = models.BigAutoField(primary_key=True)
     nom = models.CharField(max_length=100)
     pays = models.CharField(max_length=100)
@@ -10,6 +10,7 @@ class aeroports(models.Model):
     def __str__(self):
         chaine = f"l'aeroport: {self.nom} dans le pays: {self.pays} d'ID: {self.id}"
         return chaine
+
 
     def dico(self):
         return {"nom": self.nom, "pays": self.pays, "id": self.id}
